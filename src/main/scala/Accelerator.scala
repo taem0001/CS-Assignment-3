c4623ff1bc39b1f5ae82e9a5dac8afc43b75a17a
Aland2110 <Aland2110@mail.com>
2024-11-27 14:25:35 +0100

    hej

--- src/main/scala/Accelerator.scala
+++ src/main/scala/Accelerator.scala
@@ -13,12 +13,19 @@ class Accelerator extends Module {
 
   })
 
-  val idle :: read :: write :: done :: black :: neighbour :: up :: down :: left :: right = Enum(10)
+  val idle :: read :: write :: done :: black :: neighbour :: up :: down :: left :: right :: check :: Nil = Enum(11)
   val state = RegInit(idle)
   val x = RegInit(0.U(32.W))
   val y = RegInit(0.U(32.W))
   val addressReg = RegInit(0.U(32.W))
   val dataReg = RegInit(0.U(32.W))
+  val tempX = RegInit(0.U(32.W))
+  val tempY = RegInit(0.U(32.W))
+  val isWhiteUp = true.B
+  val isWhiteDown = true.B
+  val isWhiteLeft = true.B
+  val isWhiteRight = true.B
+
 
   switch(state) {
     is(idle) {
@@ -28,7 +35,8 @@ class Accelerator extends Module {
     }
     is(read) {
       io.address := addressReg
-      io.dataWrite := 0.U
+      dataReg := 0.U
+      io.dataWrite := dataReg
       io.writeEnable := false.B
       when(x === 0.U || y === 0.U || x === 19.U || y === 19.U || io.dataRead === 0.U) {
         state := black
@@ -56,6 +64,62 @@ class Accelerator extends Module {
       }
       state := done
     }
-    
+    is(neighbour) {
+      state := up
+
+
+    }
+
+    is(up) {
+      tempY := y-1.U
+      addressReg := x + 20.U * tempY
+      io.address := addressReg
+      when (io.dataRead === 255.U) {
+        isWhiteUp := true.B
+
+      } .otherwise {
+        isWhiteUp := false.B
+      }
+      state := down
+    }
+    is(down) {
+      tempY := y+1.U
+      addressReg := x + 20.U * tempY
+      io.address := addressReg
+      when (io.dataRead === 255.U) {
+        isWhiteDown := true.B
+
+      } .otherwise {
+        isWhiteDown := false.B
+      }
+      state := left
+    }
+    is(left) {
+      tempX := x-1.U
+      addressReg := tempX + 20.U * y
+      io.address := addressReg
+      when (io.dataRead === 255.U) {
+        isWhiteLeft := true.B
+
+      } .otherwise {
+        isWhiteLeft := false.B
+      }
+      state := right
+    }
+    is(right) {
+      tempX := x+1.U
+      addressReg := tempX + 20.U * y
+      io.address := addressReg
+      when (io.dataRead === 255.U) {
+        isWhiteRight := true.B
+
+      } .otherwise {
+        isWhiteRight := false.B
+      }
+      state := check
+    }
+    is (check) {
+      
+    }
   }
 }
