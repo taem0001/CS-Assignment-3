2f5137f9e8c1a8cc386169b8303360e3b87c4cfa
taem0001 <s235268@dtu.dk>
2024-12-03 19:48:11 +0100

    DET VIRKER

--- src/main/scala/Accelerator.scala
+++ src/main/scala/Accelerator.scala
@@ -13,48 +13,51 @@ class Accelerator extends Module {
 
   })
 
-  val idle :: read :: write :: done :: black :: up :: down :: left :: right :: check :: Nil = Enum(10)
+  val idle :: read :: write :: increment :: done :: up :: down :: left :: right :: checkUp :: checkDown :: checkLeft :: checkRight :: Nil = Enum(13)
   val state = RegInit(idle)
+  val x = RegInit(0.U(5.W))
+  val y = RegInit(0.U(5.W))
+  val dataReadReg = RegNext(io.dataRead)
+  val dataWriteReg = RegInit(0.U(32.W))
 
-  val addressReg = RegInit(0.U(16.W))
-  val dataReg = RegInit(0.U(32.W))
-  val x = RegInit(0.U(8.W))
-  val y = RegInit(0.U(8.W))
-
+  io.address := 0.U
   io.writeEnable := false.B
-  io.address := 0.U(16.W)
-  io.dataWrite := dataReg
+  io.dataWrite := dataWriteReg
   io.done := false.B
 
   switch(state) {
     is(idle) {
       when(io.start) {
-        state := read
-        addressReg := 0.U
         x := 0.U
         y := 0.U
+        io.address := 0.U
+        state := read
       }
     }
     is(read) {
-      io.address := addressReg
-
-      when(x === 0.U || y === 0.U || x === 19.U || y === 19.U || io.dataRead === 0.U) {
-        state := black
+      when(x === 0.U || x === 19.U || y === 0.U || y === 19.U || dataReadReg === 0.U) {
+        dataWriteReg := 0.U
+        state := write
       }.otherwise {
         state := up
       }
     }
     is(write) {
-      io.address := addressReg + 400.U
+      io.dataWrite := dataWriteReg
+      io.address := (x + 20.U * y) + 400.U
       io.writeEnable := true.B
-      addressReg := x + 20.U * y
+      state := increment
+    }
+    is(increment) {
       when(y < 19.U) {
         y := y + 1.U
       }.otherwise {
         x := x + 1.U
         y := 0.U
       }
-      when(addressReg < 399.U) {
+      io.address := x + 20.U * y
+
+      when(x < 20.U && y < 20.U) {
         state := read
       }.otherwise {
         state := done
@@ -64,49 +67,54 @@ class Accelerator extends Module {
       io.done := true.B
       state := done
     }
-    is(black) {
-      dataReg := 0.U
-      state := write
-    }
     is(up) {
       io.address := x + 20.U * (y - 1.U)
-
-      when(io.dataRead === 255.U) {
+      state := checkUp
+    }
+    is(checkUp) {
+      when(dataReadReg === 255.U) {
         state := down
       }.otherwise {
-        state := black
+        dataWriteReg := 0.U
+        state := write
       }
     }
     is(down) {
       io.address := x + 20.U * (y + 1.U)
-
-      when(io.dataRead === 255.U) {
-        state := right
-      }.otherwise {
-        state := black
-      }
+      state := checkDown
     }
-    is(right) {
-      io.address := (x + 1.U) + 20.U * y
-
-      when(io.dataRead === 255.U) {
+    is(checkDown) {
+      when(dataReadReg === 255.U) {
         state := left
       }.otherwise {
-        state := black
+        dataWriteReg := 0.U
+        state := write
       }
     }
     is(left) {
       io.address := (x - 1.U) + 20.U * y
-
-      when(io.dataRead === 255.U) {
-        state := check
+      state := checkLeft
+    }
+    is(checkLeft) {
+      when(dataReadReg === 255.U) {
+        state := right
       }.otherwise {
-        state := black
+        dataWriteReg := 0.U
+        state := write
       }
     }
-    is(check) {
-      dataReg := 255.U
-      state := write
+    is(right) {
+      io.address := (x + 1.U) + 20.U * y
+      state := checkRight
+    }
+    is(checkRight) {
+      when(dataReadReg === 255.U) {
+        dataWriteReg := 255.U
+        state := write
+      }.otherwise {
+        dataWriteReg := 0.U
+        state := write
+      }
     }
   }
 }
